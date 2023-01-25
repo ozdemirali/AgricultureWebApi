beedbea4a1e6c35276bf009ae2fd47699fa710a1
Ali <ozdemirali@hotmail.com>
2023-01-25 23:19:47 +0300

    TypeController is started

---
+++ AgricultureWebApi/Controllers/TypeController.cs
@@ -0,0 +1,34 @@
+﻿using AgricultureWebApi.Context;
+using AgricultureWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgricultureWebApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TypeController : ControllerBase
+    {
+        private readonly AgricultureDbContext _context;
+        public TypeController(AgricultureDbContext context)
+        {
+            _context = context; 
+        }
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    return Ok(_context.AgricalturalTypes.ToList());
+        //}
+
+
+        // Asecron Works
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AgricalturalType>>> GetTypeList()
+        {
+            return await _context.AgricalturalTypes.ToListAsync();
+
+        }
+    }
+}
