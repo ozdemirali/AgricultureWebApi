796e7aaacae8d5091d9c09fde17c2b349edfb67a
Ali <ozdemirali@hotmail.com>
2023-02-14 22:58:55 +0300

    Upload Controller is done

---
+++ AgricultureWebApi/Controllers/UploadController.cs
@@ -0,0 +1,61 @@
+﻿using AgricultureWebApi.Context;
+using AgricultureWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace AgricultureWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UploadController : ControllerBase
+    {
+        private readonly AgricultureDbContext _context;
+        public UploadController(AgricultureDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        public async Task<IActionResult> UploadAsync()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                //var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok("Ok");
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception e)
+            {
+                Error error = new()
+                {
+                    Id = Guid.NewGuid(),
+                    Message = e.Message,
+                    Place = "Upload File From Upload Controller",
+                    Time = DateTime.UtcNow,
+                };
+                _context.Errors.Add(error);
+                await _context.SaveChangesAsync();
+
+                return StatusCode(500, $"Internal server error: {e}");
+            }
+        }
+    }
+}
--- AgricultureWebApi/Program.cs
+++ AgricultureWebApi/Program.cs
@@ -1,5 +1,7 @@
 using AgricultureWebApi.Context;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +12,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add connectionString 
 builder.Services.AddDbContext<AgricultureDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("AgricultureDatabase")));
 
+//add for uplaod file 
+builder.Services.Configure<FormOptions>(o =>
+{
+    o.ValueLengthLimit = int.MaxValue;
+    o.MultipartBodyLengthLimit = int.MaxValue;
+    o.MemoryBufferThreshold = int.MaxValue;
+});
+//--------------------
 
 
 var app = builder.Build();
@@ -30,4 +41,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//add for upload file
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+    RequestPath = new PathString("/Resources")
+});
+//-----------
 app.Run();
